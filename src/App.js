56f3bb58c80bdc9b9013095e6fc6b634e617e5eb
divanshu <sharmadivanshu008@gmail.com>
2024-08-21 15:54:07 +0530

    setup app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mic_icon from "./assets/images/icons/mic.svg"
+import leave_icon from "./assets/images/icons/leave.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="room-header">
+        <h1 id="room-name"></h1>
+
+        <div id="room-header-controls">
+          <img id="mic-icon" class="control-icon" src={mic_icon} />
+          <img id="leave-icon" class="control-icon" src={leave_icon} />
+        </div>
+      </div>
+
+      <form id="form">
+        {/* <input name="displayname" type="text" placeholder="Enter display name..."/>  */}
+        <input type="submit" value="Enter Room" />
+      </form>
+
+      <div id="members"></div>
     </div>
   );
 }
